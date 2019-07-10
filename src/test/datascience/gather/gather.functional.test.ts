785fd41f302ba0cef0237f648bc04174eb58394a
joycehle <joyceerhuiling@gmail.com>
2019-07-10 10:19:19 -0700

    Rename to *.unit.test.ts

---
+++ src/test/datascience/gather/gather.functional.test.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+
+suite('DataScience code gathering tests', () => {
+    test('Log a cell execution', async () => {
+
+    });
+
+    test('Gather program slices for a cell', () => {
+
+    });
+
+});
