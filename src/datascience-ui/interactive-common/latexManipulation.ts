d6f37794552909bc274efd3d3617b8b47428f8e8
David Kutugata <dakutuga@microsoft.com>
2019-10-25 16:05:41 -0700

    added missing copyright

--- src/datascience-ui/interactive-common/latexManipulation.ts
+++ src/datascience-ui/interactive-common/latexManipulation.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 // Adds '$$' to latex formulas that don't have a '$', allowing users to input the formula directly.
 function fixLatexEquations(input: string): string {
     const block = '\n$$\n';
